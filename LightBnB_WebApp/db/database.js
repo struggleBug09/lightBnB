35345ed327027c8616718709bee8cda779fab8b2
Jan Edmark Arieta <edarieta09@gmail.com>
2024-03-29 11:21:15 +0000

    reserveations editted

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -80,8 +80,28 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryText = `
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date DESC
+    LIMIT $2;
+  `;
+  return pool
+    .query(queryText, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 };
+
 
 /// Properties
 
